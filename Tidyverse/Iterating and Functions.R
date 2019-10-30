dea0b756eea5ec683175b628f94cf6d2b2df1465
Edgar Zamora <edgarzamora@Edgars-MacBook-Pro.local>
2019-10-30 12:17:41 -0700

    Add new sample.

--- Tidyverse/Iterating and Functions.R
+++ Tidyverse/Iterating and Functions.R
@@ -1,1 +1,24 @@
+# Libaries ----
+library(tidyverse)
 
+# Loading Data ----
+df <- read.csv("FallSurveyclean v2.csv") %>% 
+  select(know_acadm_advisor, academ_advisor_availability, academ_advisor_knowledge, academ_advisor_trans_knowledge, 
+         academ_advisor_concerned, degreecert_require_clear, plan_complete_degreecert) 
+
+# Function for percentages ----
+cal_percentage <- function(question){
+  df %>% 
+    group_by({{question}}) %>% 
+    summarize(n = n()) %>% 
+    mutate(percent = paste0(round(100 * n/sum(n),1), "%"))
+}
+
+
+# Iterating through all the values and store----
+Calculations <- df %>% 
+  map(cal_percentage)
+
+
+# Exporting file ----
+write.csv(Calculations, "Fall 2018 Survey- Advising Calculations.csv", row.names = FALSE)
