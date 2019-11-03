699b6aa286a837ccd4c2175a2fe8f28093517123
Edgar-Zamora <edgarzamora2012@hotmail.com>
2019-11-03 14:23:54 -0800

    Files for US House vote on impeachment.

---
+++ Tidyverse/House Vote.R
@@ -0,0 +1,98 @@
+library(tidyverse)
+library(rvest)
+library(stringr)
+install.packages("statebins", repos = "https://cinc.rud.is")
+library(statebins)
+
+#Getting URLS
+url <- "https://www.nytimes.com/interactive/2019/10/31/us/politics/trump-impeachment-inquiry-house-vote.html?module=inline"
+
+table <- url %>% 
+  read_html() %>% 
+  html_nodes(".g-multi-table")
+
+names <- table %>% 
+  html_nodes(".g-name") %>% 
+  html_text() %>% 
+  tibble() %>% 
+  rename("Names" = ".") %>% 
+  slice(-1, -199, -434)
+
+votes <- table %>% 
+  html_nodes(".g-cell-block") %>% 
+  html_text() %>%
+  tibble() %>% 
+  rename("Votes" = ".")
+
+votes <- str_trim(votes$Votes, side = "both") 
+
+votes <- votes %>% 
+  tibble() %>% 
+  rename("Votes" = ".") 
+
+
+#### New Column for Party
+Rep <- (1:197)
+Demo <- (198:431)
+Ind <- 432
+
+names <- names %>% 
+  mutate(Party = ifelse(seq_len(nrow(names)) %in% Rep, "R",
+         ifelse(seq_len(nrow(names)) %in% Demo, "D", "I")))
+
+complete <- bind_cols(names, votes) %>% 
+  separate(Names,c("Names","State","District"),"\\s(?=\\S+\\s+(?=\\d))|\\s+(?=\\d)") %>% 
+  mutate(State = recode(State,
+                        Ala. = "Alabama", Alaska = "Alaska", Ariz. = "Arizona",
+                        Ark.  = "Arkansas", Calif. = "California", Colo. = "Colorado",
+                        Conn. = "Connecticut",Del.  = "Delaware", Fla.  = "Florida",
+                        Ga.   = "Georgia", Hawaii = "Hawaii", Idaho = "Idaho",
+                        Ill.  = "Illinois", Ind.  = "Indiana",Iowa  = "Iowa",
+                        Kan.  = "Kansas", Ky.   = "Kentucky",La.   = "Louisiana",
+                        Mass. = "Massachusetts", Md.   = "Maryland", Me.   = "Maine",
+                        Mich. = "Michigan", Minn. = "Minnesota",Miss. = "Mississippi", 
+                        Mo.   = "Missouri", Mont. = "Montana", N.C.  = "North Carolina",  
+                        N.D.  = "North Dakota", N.H.  = "New Hampshire", N.J.  = "New Jersey",
+                        N.M.  = "New Mexico", N.Y.  = "New York", Neb.  = "Nebraska",
+                        Nev.  = "Nevada", Ohio  = "Ohio",  Okla. = "Oklahoma",
+                        Ore.  = "Oregon",Pa.   = "Pennsylvania",R.I.  = "Rhode Island",
+                        S.C.  = "South Carolina",S.D.  = "South Dakota",Tenn. = "Tennessee",
+                        Tex.  = "Texas",Utah  = "Utah",  Va.   = "Virginia",
+                        Vt.   = "Vermont", W.Va. = "West Virginia", Wash. = "Washington",
+                        Wis.  = "Wisconsin",Wyo.  = "Wyoming"),
+         Votes = recode(Votes,
+                        Y = 1,
+                        N = -1,
+                        NV = 0))
+
+x <- complete %>% 
+  group_by(State) %>% 
+  summarize(Total = sum(Votes)) %>% 
+  mutate(Party = if_else(Total > 0, "D", "R")) %>% 
+  print(n = Inf)
+
+statebins(state_data = x, value_col = "Total", state_col = "State", 
+          round = TRUE,  direction = 2) +
+  ggplot2_scale_function = scale_color_gradient2(low = "red", midpoint = 3, mid = "blue", high = "green") +
+  theme_statebins(legend_position="right")
+
+mid <- mean(x$Total)
+
+ggplot(x, aes(state = State, fill = Total)) +
+  geom_statebins(border_col="grey90", border_size = .2) +
+  labs(title = "How The House Voted On The Trump Impeachment Rule",
+       subtitle = 'A "Ya" is a 1 while a "Nay" is -1 with non-voters as 0') +
+  scale_fill_gradient2(low = "red", high = "blue", midpoint = -1) +
+  theme(panel.background = element_blank(),
+        axis.ticks = element_blank(), 
+        axis.text = element_blank(),
+        plot.title = element_text(size = 23))
+
+x %>% 
+  group_by(State,Total) %>% 
+  arrange(desc(Total)) %>% 
+  print(n = Inf)
+
+
+
+
