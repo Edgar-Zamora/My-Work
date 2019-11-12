d83d91d6a63e74d6f81cea125b50e7c16a913a06
Edgar-Zamora <edgarzamora2012@hotmail.com>
2019-11-12 09:38:35 -0800

    Final updates to TidyTuesday (11/5)

--- #TidyTuesday/Modes Less Traveled/Modes Less Traveled .R
+++ #TidyTuesday/Modes Less Traveled/Modes Less Traveled .R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(statebins)
 library(viridis)
 library(scales)
-#options(scipen=999)
+options(scipen=999)
 
 #Load Data
 commute_mode <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-11-05/commute.csv")
@@ -15,19 +15,16 @@ total_walking_num <- commute_mode %>%
   summarize(walking_num = sum(n))
 
 #Visualize total number of walkers per state using the statebins package
-ggplot(total_walking_num, aes(state = state_abb, fill = log(walking_num))) +
+ggplot(total_walking_num, aes(state = state_abb, fill = log2(walking_num))) +
   geom_statebins(border_col="grey40", border_size = .2) +
   scale_fill_viridis(option = "magma", direction = -1, label = comma,
                      name = "") +
   labs(title = "Do you like walking to places?",
-       subtitle = "Aggregate number of walkers by state") +
+       subtitle = "Aggregate (log) number of walkers by state") +
   theme(panel.background = element_blank(),
         plot.subtitle = element_text(size = 8),
         legend.position = "right",
         axis.ticks = element_blank(), 
         axis.text = element_blank(),
-        plot.title = element_text(size = 24)) +
+        plot.title = element_text(size = 23.5)) +
   guides(fill = guide_colourbar(barwidth = 0.7, barheight = 8))
-
-
-
