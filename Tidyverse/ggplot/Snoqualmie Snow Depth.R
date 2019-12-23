2b8ba26d13582912df40ceda447f341b5efe511e
Edgar-Zamora <edgarzamora2012@hotmail.com>
2019-12-22 20:28:23 -0800

    new graphic

---
+++ Tidyverse/ggplot/Snoqualmie Snow Depth.R
@@ -0,0 +1,42 @@
+#Packages
+#install.packages("googlesheets4")
+#installed.packages("googledrive")
+#install.packages("gghighlight")
+
+library(googledrive)
+library(googlesheets4)
+library(tidyverse)
+library(gghighlight)
+
+sheets_auth()
+
+deaths <- drive_get("WSDOT_Mountain_Pass_Snow_Level")
+
+snow_levels <- deaths %>% 
+  read_sheet() %>% 
+  mutate(Year = as.factor(Year),
+         Month = fct_relevel(Month, c("October","November", "December",
+                                      "January", "Feburary", "March", "April",
+                                      "May")))
+
+glimpse(snow_levels)  
+
+snow_levels %>% 
+  filter(Year == "18-19") %>% 
+  ggplot(aes(Day, New, group = Month, colour = Month)) + 
+  geom_line(key_glyph = "timeseries", size = .8) +
+  gghighlight(class == "Feburary") +
+  scale_y_continuous(limits = c(0,35),
+                     breaks = seq(0,35,5)) +
+  scale_x_continuous(limits = c(0,31),
+                     breaks = seq(0,31, 5)) +
+  labs(title = "New Snow on Snoqualmie Pass by Month and Day
+       ",
+       y = "Total (ft)") +
+  theme(axis.ticks = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.grid.major.y = element_line(colour = "grey90", size = .2),
+        panel.grid.major.x = element_blank(), 
+        panel.background = element_blank(),
+        legend.key = element_blank())
+
