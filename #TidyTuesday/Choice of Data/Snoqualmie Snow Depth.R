3a443ca6d265c933323333ab09692fd3a30c23f8
Edgar-Zamora <edgarzamora2012@hotmail.com>
2020-01-06 14:21:42 -0800

    New folder and TidyTuesday

--- #TidyTuesday/Choice of Data/Snoqualmie Snow Depth.R
+++ #TidyTuesday/Choice of Data/Snoqualmie Snow Depth.R
@@ -9,39 +9,45 @@ library(tidyverse)
 library(gghighlight)
 library(Hmisc)
 
+## Connecting to Google Account and Google Sheets
+# Authroization
 sheets_auth()
 
-deaths <- drive_get("WSDOT_Mountain_Pass_Snow_Level")
+# Linking to Google Drive
+WSDOT_snow_levels <- drive_get("WSDOT_Mountain_Pass_Snow_Level")
 
-snow_levels <- deaths %>% 
+#Importing data and creating ranges
+snow_levels <- WSDOT_snow_levels %>% 
   read_sheet() %>% 
   mutate(Year = as.factor(Year),
          Month = fct_relevel(Month, c("October","November", "December",
                                       "January", "Feburary", "March", "April",
-                                      "May")))
-#glimpse(snow_levels)  
-x <- snow_levels %>% 
+                                      "May"))) %>% 
   mutate(new_group = ifelse(New > 10, "10+", 
                             ifelse(New > 6.0, "6-9.9",
                                    ifelse(New > 3.0, "3-5.9", "0-2.9"))))
 
 cols <- c("10+" = "#27408B", "6-9.9" = "#1874CD", "3-5.9" = "#7B9AFA", "0-2.9" = "#CFCFCF")
 
-x %>% 
+snow_levels %>% 
   filter(Year == "18-19") %>% 
-  ggplot(aes(Day, Month, group = Month, fill = new_group)) + 
+  ggplot(aes(Day, fct_rev(Month), group = Month, fill = new_group)) + 
   geom_tile(color = "white", size = .25) +
   scale_fill_manual(values = cols,
-                    name = "Snow Lvl.",
+                    name = "Snow Lvl. (in)",
                     limits = c("0-2.9", "3-5.9", "6-9.9", "10+")) +
   scale_x_continuous(limits = c(0,33),
                      breaks = seq(1,33, 3)) +
-  labs(title = "New Snow on Snoqualmie Pass",
-       y = "") +
+  labs(title = "Daily New Snow on Snoqualmie Pass",
+       y = "",
+       x = "") +
   theme(axis.ticks = element_blank(),
-        axis.text.y = element_text(hjust = 0),
-        plot.title = element_text(size = 15, hjust = .5),
+        axis.text.y = element_text(hjust = 0, size = 11, colour = "black"),
+        axis.text.x = element_text(size = 9, colour = "black"),
+        plot.title = element_text(size = 16, hjust = .5),
         panel.grid.minor = element_blank(),
         panel.grid.major = element_blank(), 
         panel.background = element_blank(),
-        legend.key = element_blank())
+        legend.key = element_blank(),
+        legend.position = "bottom",
+        legend.box.spacing = unit(-0.5, "cm"))
