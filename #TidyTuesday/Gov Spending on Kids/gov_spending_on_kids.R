4c46fc56fd8bb67c71782c041d8e66ba646e7677
Edgar-Zamora <edgarzamora2012@hotmail.com>
2020-09-17 16:23:28 -0700

    tidytuesday

--- #TidyTuesday/Gov Spending on Kids/gov_spending_on_kids.R
+++ #TidyTuesday/Gov Spending on Kids/gov_spending_on_kids.R
@@ -3,35 +3,52 @@ library(janitor)
 library(geofacet)
 library(scales)
 
-kids <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-15/kids.csv')
+kids <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-15/kids.csv') %>% 
+  left_join(tibble(
+  state_abb = state.abb,
+  state_name = state.name) %>% 
+  add_row(state_abb = "DC", state_name = "District of Columbia"), by = c("state" = "state_name"))
 
-
-top_5_spending_areas <- kids %>%
-  group_by(state, variable) %>% 
+state_spending <- kids %>% 
+  filter(variable == "highered") %>% 
+  group_by(state_abb, year) %>% 
   summarise(
-    var_spending = sum(raw),
-  ) %>% 
+    avg = round(mean(inf_adj_perchild), 2)) %>% 
   ungroup() %>% 
-  group_by(state) %>%
-  mutate(total_state_spending = sum(var_spending, na.rm = T),
-         percent_var_total = var_spending/total_state_spending * 100) %>%
-  top_n(5, percent_var_total) %>%
-  ungroup()
+  group_by(state_abb) %>% 
+  mutate(diff = avg - lag(avg, default = first(avg))) %>% 
+  ungroup() %>% 
+  mutate(
+    spending_change = case_when(diff == 0 ~ "Same",
+                       diff < 0 ~ "Decrease",
+                       TRUE ~ "Increase"),
+    spending_change = factor(spending_change, levels = c("Increase", "Same", "Decrease")))
 
 
-
-top_5_spending_areas %>% 
-  filter(state == "Washington") %>% 
-  ggplot(aes(percent_var_total, "", fill = variable)) +
-  geom_col() +
-  scale_x_continuous(limits = c(0, 100)) 
-facet_geo(~state, grid = "us_state_grid2") +
+state_spending %>% 
+  #filter(state == "Washington") %>% 
+  ggplot(aes(year, avg, group = state_abb, color = spending_change)) +
+  geom_line(size = .8, key_glyph = "timeseries") +
+  scale_color_manual(name = "Spending Change", values = c("#59A14F","#BAB0AC", "#E15759")) +
+  scale_y_continuous(labels = dollar_format()) +
   labs(
-    y = NULL,
     x = NULL,
-    title = "Show Me the Money (or lack of)",
-    subtitle = "Top 5 spending areas for each state from x to x") +
+    y = "Adjusted for Inflation Per Child ($)"
+  ) +
   theme(
+    legend.key = element_blank(),
+    panel.grid = element_blank(),
+    panel.background = element_blank(),
     axis.ticks = element_blank(),
-    strip.text.x = element_text(size = 6))
+    axis.text.x = element_blank()
+  ) +
+  facet_geo(~state_abb, grid = "us_state_grid2")
+
+
+
+
+
+
+
+
 
