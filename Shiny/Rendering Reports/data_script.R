7600baf54c69ceac989ec061e4c9018713d255e2
Edgar-Zamora <edgarzamora2012@hotmail.com>
2021-12-16 23:24:32 -0800

    Plot funs.

--- Shiny/Rendering Reports/data_script.R
+++ Shiny/Rendering Reports/data_script.R
@@ -12,33 +12,51 @@ session <- bow("https://www.baseball-reference.com")
 
 
 # Reading table
-mariners_season <- read_html("https://www.baseball-reference.com/teams/SEA/2019-schedule-scores.shtml") %>% 
-  html_element("table") %>% 
-  html_table() %>% 
-  clean_names() %>% 
-  rename(
-    day_night = d_n
-  ) %>% 
-  filter(str_detect(gm_number, "[:digit:]")) %>% 
-  separate(col = date, into = c('weekday', 'date'), sep = ',') %>%  
-  mutate(attendance = str_remove(attendance, ","),
-         across(c(r, ra, attendance, c_li), as.numeric), 
-         across(date, str_trim),
-         #date =  format(mdy(paste(date, '2019')), "%m/%d/%Y"),
-         home_away = case_when(x_2 == "@" ~ "Away",
-                               TRUE ~ "Home"),
-         extra_innings = case_when(inn != "" ~ 1,
-                                   TRUE ~ 0),
-         games_back2 = case_when(str_detect(gb, "up") ~ as.numeric(str_extract(gb, "[:digit:].+")),
-                         gb == 'Tied' ~ 0,
-                         TRUE ~ as.numeric(gb) * -1),
-         run_diff = r - ra,
-         ate =  format(mdy(paste(date, '2019')), "%m/%d/%Y")) %>% 
 
-  select(-c(x, x_2)) %>% 
-  rename(
-    runs_for = r,
-    runs_against = ra,
-    games_back = gb
-  )
+mlb_team_schedule <- function(team, year) {
+  
+  team_url <- paste0('https://www.baseball-reference.com/teams/', {{team}}, "/", {{year}},"-schedule-scores.shtml")
+  
+  team_schedule <- read_html(team_url) %>% 
+    html_element("table") %>% 
+    html_table() %>% 
+    clean_names() %>% 
+    rename(
+      day_night = d_n
+    ) %>% 
+    filter(str_detect(gm_number, "[:digit:]")) %>% 
+    separate(col = date, into = c('weekday', 'date'), sep = ',') %>%  
+    mutate(attendance = str_remove(attendance, ","),
+           across(c(r, ra, attendance, c_li), as.numeric), 
+           across(date, str_trim),
+           home_away = case_when(x_2 == "@" ~ "Away",
+                                 TRUE ~ "Home"),
+           extra_innings = case_when(inn != "" ~ 1,
+                                     TRUE ~ 0),
+           games_back2 = case_when(str_detect(gb, "up") ~ as.numeric(str_extract(gb, "[:digit:].+")),
+                                   gb == 'Tied' ~ 0,
+                                   TRUE ~ as.numeric(gb) * -1),
+           run_diff = r - ra,
+           team = {{team}},
+           double_header_gm = str_extract(date, "(?<=\\().*(?=\\))"),
+           date = str_trim(str_remove(date, "\\(([^\\)]+)\\)")),
+           double_header = case_when(is.na(double_header_gm) ~ 0,
+                                     TRUE ~ 1),
+           date =  as.Date(mdy(paste(date, {{year}})), "%Y%m%d"),
+    ) %>% 
+    select(-c(x, x_2)) %>% 
+    rename(
+      runs_for = r,
+      runs_against = ra,
+      games_back = gb
+    )
+  
+  return(team_schedule)
+  
+}
+
+
+
+
+
 
--- Shiny/Rendering Reports/helper_funs.R
+++ Shiny/Rendering Reports/helper_funs.R
@@ -1,6 +1,9 @@
 # Packages
 library(tidyverse)
 library(janitor)
+library(glue)
+library(tidytext)
+library(scales)
 
 
 # Year data
@@ -34,13 +37,58 @@ annual_facts <- mlb_2019 %>%
 
 
 ## Yearly Attendance
-mlb_2019 %>% 
-  filter(team == 'NYY',
-         home_away == 'Home') %>% 
-  ggplot(aes(date, attendance, group = 1)) +
-  geom_hline(yintercept = 41827) +
-  geom_line() +
-  scale_y_continuous(labels = scales::comma)
+yearly_attnd <- function(team, year){
+  
+  mlb_2019 %>% 
+    filter(team == {{team}}) %>% 
+    mutate(month = month(date)) %>% 
+    ggplot(aes(date, attendance)) +
+    geom_line(size = .75) +
+    geom_point(aes(color = home_away), size = 3) +
+    scale_color_manual(values = c("#c8d0d9", "#1170aa"),
+                       name = 'Home or Away?') +
+    scale_y_continuous(labels = scales::comma) +
+    scale_x_date(date_labels = "%b %d",
+                 date_breaks = "2 weeks") +
+    labs(
+      x = "",
+      y = "Game Attendance",
+      title = glue("{year} {team} Attendance")
+    )
+  
+  
+}
+
+
+yearly_attnd("LAD", year)
+
+
+
+## Top 5 Opponents
+x <- mlb_2019 %>% 
+  filter(team == 'NYY') %>% 
+  select(opp, attendance, home_away) %>% 
+  group_by(home_away, opp) %>% 
+  summarise(
+    avg_attendance = mean(attendance, na.rm = T),
+    num_games = n()
+  ) %>% 
+  ungroup() %>% 
+  group_by(home_away) %>% 
+  arrange(desc(avg_attendance)) %>% 
+  slice(1:5)
+
+
+
+x %>% 
+  ungroup() %>% 
+  mutate(opp = reorder_within(opp, avg_attendance, home_away)) %>% 
+  ggplot(aes(avg_attendance, opp)) +
+  geom_col() +
+  geom_text(aes(label = paste0(comma(round(avg_attendance, 0)), " (", num_games,")")),
+            hjust = 3, color = "white") +
+  facet_grid(rows = vars(home_away), scales = "free_y") +
+  scale_y_reordered()
 
 
 
