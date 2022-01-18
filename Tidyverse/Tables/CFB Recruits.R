bedb99bacb28d1c595d617386dee3b6de2a67f4e
Edgar-Zamora <edgarzamora2012@hotmail.com>
2022-01-17 23:26:48 -0800

    adding top schools and logos

--- Tidyverse/Tables/CFB Recruits.R
+++ Tidyverse/Tables/CFB Recruits.R
@@ -30,8 +30,8 @@ cfb_2022_recruits <- results %>%
 
 
 
-
-# Helper Functions/DF
+# Helper Functions/dataframes
+## State name lookup
 state_df <- tibble(
   state_abb = state.abb,
   state_name = state.name
@@ -39,10 +39,12 @@ state_df <- tibble(
   mutate(across(state_name, str_to_lower))
 
 
+## State outline data
 state_map_data <- map_data("state") %>% 
   as_tibble()
 
 
+## Function for state outlines
 state_outline <- function(data, region) {
   
   data %>% 
@@ -59,6 +61,32 @@ state_outline <- function(data, region) {
     )
   
 }
+
+## School logos
+school_logos <- cfbfastR::cfbd_team_info(year = 2022) %>% 
+  as_tibble() %>% 
+  unnest(logos) %>% 
+  group_by(school) %>% 
+  filter(str_detect(logos, "\\bdark\\b", negate = TRUE)) %>% 
+  select(school, logos) %>% 
+  ungroup() %>% 
+  add_row(school = c("Central Arkansas", "Montana State", "North Dakota State", "Jackson State", "Northern Arizona",
+                     "Northern Iowa", "Weber State", "Murray State", "Southern Illinois", "Eastern Washington",
+                     "Princeton", "Monmouth", "Bucknell", "Villanova"),
+          logos = c("http://a.espncdn.com/i/teamlogos/ncaa/500/2110.png",
+                    "http://a.espncdn.com/i/teamlogos/ncaa/500/149.png",
+                    "https://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/2449.png?w=110&h=110&transparent=true",
+                    "http://a.espncdn.com/i/teamlogos/ncaa/500/2296.png",
+                    "http://a.espncdn.com/i/teamlogos/ncaa/500/2464.png",
+                    "http://a.espncdn.com/i/teamlogos/ncaa/500/2460.png",
+                    "http://a.espncdn.com/i/teamlogos/ncaa/500/2692.png",
+                    "http://a.espncdn.com/i/teamlogos/ncaa/500/93.png",
+                    "http://a.espncdn.com/i/teamlogos/ncaa/500/79.png",
+                    "http://a.espncdn.com/i/teamlogos/ncaa/500/331.png",
+                    "http://a.espncdn.com/i/teamlogos/ncaa/500/163.png",
+                    "http://a.espncdn.com/i/teamlogos/ncaa/500/2405.png",
+                    "http://a.espncdn.com/i/teamlogos/ncaa/500/2083.png",
+                    "http://a.espncdn.com/i/teamlogos/ncaa/500/222.png"))
 
 
 
@@ -90,6 +118,7 @@ cfb_2022_plyr_recruits <- cfb_2022_recruits %>%
 
 
 
+### State Summary ### 
 # Summarizing state information
 cfb_state_summary <- cfb_2022_plyr_recruits %>% 
   filter(state_province %in% state.abb) %>% 
@@ -102,6 +131,60 @@ cfb_state_summary <- cfb_2022_plyr_recruits %>%
   ) %>% 
   ungroup()
   
+
+# Finding top 3 schools for each category
+top3_schls <- cfb_2022_plyr_recruits %>% 
+  filter(!is.na(committed_to),
+         state_province %in% state.abb) %>% 
+  group_by(state_province, committed_to) %>% 
+  summarise(
+    commits = n()
+  ) %>% 
+  slice_max(order_by = commits, n = 3) %>% 
+  ungroup()  %>% 
+  left_join(school_logos, by = c("committed_to" = "school")) %>% 
+  group_by(state_province) %>% 
+  mutate(top3_schls = paste0("<div class='top-3'> <img class='top-3-img' src='", logos, "'> <span class='top-3-txt'>",committed_to, " (<strong>", commits, "</strong>) </span>
+                       </div>", collapse = ""))  %>% 
+  ungroup() %>% 
+  distinct(state_province, top3_schls) 
+
+
+in_state_top3_schls <- cfb_2022_plyr_recruits %>% 
+  filter(!is.na(committed_to),
+         in_state == "y",
+         state_province %in% state.abb) %>% 
+  group_by(state_province, committed_to) %>% 
+  summarise(
+    commits = n()
+  ) %>% 
+  slice_max(order_by = commits, n = 3) %>% 
+  ungroup()  %>% 
+  left_join(school_logos, by = c("committed_to" = "school")) %>% 
+  group_by(state_province) %>% 
+  mutate(in_state_top3_schls = paste0("<div class='top-3'> <img class='top-3-img' src='", logos, "'> <span class='top-3-txt'>",committed_to, " (<strong>", commits, "</strong>) </span>
+                       </div>", collapse = ""))  %>% 
+  ungroup() %>% 
+  distinct(state_province, in_state_top3_schls) 
+
+
+
+top3_schls_300 <- cfb_2022_plyr_recruits %>% 
+  filter(!is.na(committed_to),
+         top300 == "y",
+         state_province %in% state.abb) %>% 
+  group_by(state_province, committed_to) %>% 
+  summarise(
+    commits = n()
+  ) %>% 
+  slice_max(order_by = commits, n = 3) %>% 
+  ungroup()  %>% 
+  left_join(school_logos, by = c("committed_to" = "school")) %>% 
+  group_by(state_province) %>% 
+  mutate(top300_schls = paste0("<div class='top-3'> <img class='top-3-img' src='", logos, "'> <span class='top-3-txt'>",committed_to, " (<strong>", commits, "</strong>) </span>
+                       </div>", collapse = ""))  %>% 
+  ungroup() %>% 
+  distinct(state_province, top300_schls) 
 
 
 
@@ -133,17 +216,19 @@ plots <- cfb_state_summary %>%
 
 
 cfb_state_summary %>% 
+  left_join(top3_schls, by = c("state_province")) %>% 
   left_join(state_df, by = c("state_province" = 'state_abb')) %>% 
+  left_join(in_state_top3_schls, by = "state_province") %>% 
+  left_join(top3_schls_300, by = "state_province") %>% 
   filter(state_province != "HI") %>% 
   mutate(ggplot = NA,
          across(state_name, str_to_title),
-         in_state_top_10 = NA,
-         in_state_top_schls = NA,
-         top300_top_schls = NA) %>% 
-  select(ggplot, state_name, num_recruits, 
-         in_state_commits, in_state_top_10, in_state_top_schls,
-         top300_recruits, top300_in_state_commits, top300_top_schls) %>% 
+         in_state_top_10 = NA) %>% 
+  select(ggplot, state_name, num_recruits, top3_schls,
+         in_state_commits, in_state_top_10, in_state_top3_schls,
+         top300_recruits, top300_in_state_commits, top300_schls) %>% 
   gt() %>% 
+  fmt_markdown(columns = c(top3_schls, in_state_top3_schls, top300_schls)) %>% 
   text_transform(
     locations = cells_body(columns = ggplot),
     fn = function(x) {
@@ -155,12 +240,13 @@ cfb_state_summary %>%
     state_name = "",
     ggplot = "",
     num_recruits = "# of Recruits",
+    top3_schls = "Top Schools",
     in_state_commits = "Commits",
     in_state_top_10 = "Top 10 Commits",
-    in_state_top_schls = "Top Schools",
+    in_state_top3_schls = "Top Schools",
     top300_recruits = "Recruits",
     top300_in_state_commits = "In-State Commits",
-    top300_top_schls = 'Top Schools'
+    top300_schls = 'Top Schools'
   ) %>% 
   tab_header(
     title = "Are They Staying Home?",
@@ -187,7 +273,7 @@ cfb_state_summary %>%
     )
   ) %>% 
   cols_align(
-    columns = 2:6,
+    columns = 2:10,
     align = "center"
   ) %>% 
   tab_options(table.font.size = px(20)) %>%
@@ -196,6 +282,24 @@ cfb_state_summary %>%
       google_font(name = "Ubuntu"),
       "Sans", "Serif"
     )
-  )
+  ) %>% 
+opt_css(
+  css = '.top-3-img {
+  width: 50px;
+  height: 50px;}
+
+  .top-3 {
+  display: flex;
+  align-items: center;}
+
+
+  .top-3-txt {
+  margin-left: 10px;}
+
+
+  .top-3-img {
+  margin: 3px 0px;
+  }'
+)
 
 
