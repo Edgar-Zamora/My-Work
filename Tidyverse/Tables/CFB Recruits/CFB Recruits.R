1ff9706ecd66084954d854501a4a9c9a5e5b9231
Edgar-Zamora <edgarzamora2012@hotmail.com>
2022-01-30 13:47:33 -0800

    CFB Recruit Tbl

--- Tidyverse/Tables/CFB Recruits/CFB Recruits.R
+++ Tidyverse/Tables/CFB Recruits/CFB Recruits.R
@@ -6,8 +6,7 @@ library(jsonlite)
 library(maps)
 library(gt)
 library(janitor)
-
-
+library(scales)
 
 
 # College Football 2022 Recruits
@@ -74,7 +73,7 @@ school_logos <- cfbfastR::cfbd_team_info(year = 2022) %>%
                      "Northern Iowa", "Weber State", "Murray State", "Southern Illinois", "Eastern Washington",
                      "Princeton", "Monmouth", "Bucknell", "Villanova"),
           logos = c("http://a.espncdn.com/i/teamlogos/ncaa/500/2110.png",
-                    "http://a.espncdn.com/i/teamlogos/ncaa/500/149.png",
+                    "https://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/147.png&w=100&h=100&transparent=true",
                     "https://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/2449.png?w=110&h=110&transparent=true",
                     "http://a.espncdn.com/i/teamlogos/ncaa/500/2296.png",
                     "http://a.espncdn.com/i/teamlogos/ncaa/500/2464.png",
@@ -87,7 +86,6 @@ school_logos <- cfbfastR::cfbd_team_info(year = 2022) %>%
                     "http://a.espncdn.com/i/teamlogos/ncaa/500/2405.png",
                     "http://a.espncdn.com/i/teamlogos/ncaa/500/2083.png",
                     "http://a.espncdn.com/i/teamlogos/ncaa/500/222.png"))
-
 
 
 # Wrangling data
@@ -126,6 +124,7 @@ cfb_state_summary <- cfb_2022_plyr_recruits %>%
   summarise(
     num_recruits = n(),
     in_state_commits = sum(in_state == "y"),
+    in_state_top = sum(in_state == "y" & state_top_10 == "y"),
     top300_recruits = sum(top300 == "y"),
     top300_in_state_commits = sum(top300 == 'y' & in_state == 'y')
   ) %>% 
@@ -189,24 +188,26 @@ top3_schls_300 <- cfb_2022_plyr_recruits %>%
 
 
 
-# Plotting
+# Plotting state outline
 plots <- cfb_state_summary %>% 
   filter(state_province != "HI") %>% 
   left_join(state_df, by = c("state_province" = 'state_abb')) %>% 
   left_join(state_map_data, by = c("state_name" = "region")) %>% 
+  left_join(state_fill_colors, by = "state_province") %>% 
   mutate(region = state_name) %>% 
   relocate(region, .before = subregion) %>%
   select(-c(num_recruits:top300_in_state_commits)) %>% 
-  nest(data = c(long:subregion)) %>% 
+  nest(data = c(long:subregion, color)) %>% 
   mutate(plot = map2(data, state_name, ~ggplot(., mapping = aes(long, lat, group = group)) +
                        coord_fixed(1.3) +
-                       geom_polygon(color="black", fill="transparent", size = 1.5) +
+                       geom_polygon(color = "black", size = 2, fill = "transparent") +
                        theme(
                          panel.grid = element_blank(),
                          panel.background = element_blank(),
                          axis.text = element_blank(),
                          axis.ticks = element_blank(),
-                         axis.title = element_blank()
+                         axis.title = element_blank(),
+                         legend.position = 'none'
                        )
                      )
          )
@@ -215,34 +216,39 @@ plots <- cfb_state_summary %>%
 
 
 
-cfb_state_summary %>% 
+gt_tbl <- cfb_state_summary %>% 
   left_join(top3_schls, by = c("state_province")) %>% 
   left_join(state_df, by = c("state_province" = 'state_abb')) %>% 
   left_join(in_state_top3_schls, by = "state_province") %>% 
   left_join(top3_schls_300, by = "state_province") %>% 
   filter(state_province != "HI") %>% 
   mutate(ggplot = NA,
-         across(state_name, str_to_title),
-         in_state_top_10 = NA) %>% 
+         across(state_name, str_to_title)
+         ) %>% 
   select(ggplot, state_name, num_recruits, top3_schls,
-         in_state_commits, in_state_top_10, in_state_top3_schls,
+         in_state_commits, in_state_top, in_state_top3_schls,
          top300_recruits, top300_in_state_commits, top300_schls) %>% 
+  
+  
+  
+  
+  # Building gt() tbl
   gt() %>% 
   fmt_markdown(columns = c(top3_schls, in_state_top3_schls, top300_schls)) %>% 
   text_transform(
     locations = cells_body(columns = ggplot),
     fn = function(x) {
       plots$plot %>% 
-        ggplot_image(height = px(150))
+        ggplot_image(height = px(175))
     }
   ) %>% 
   cols_label(
     state_name = "",
     ggplot = "",
-    num_recruits = "# of Recruits",
+    num_recruits = "Total Recruits",
     top3_schls = "Top Schools",
     in_state_commits = "Commits",
-    in_state_top_10 = "Top 10 Commits",
+    in_state_top = "Top 10 Commits",
     in_state_top3_schls = "Top Schools",
     top300_recruits = "Recruits",
     top300_in_state_commits = "In-State Commits",
@@ -252,6 +258,12 @@ cfb_state_summary %>%
     title = "Are They Staying Home?",
     subtitle = "Looking at whether college recruits commit to in-state schools"
   ) %>% 
+  cols_align(
+    columns = 2:10,
+    align = "center"
+  ) %>% 
+  
+  # Adding table spanners
   tab_spanner(
     label = "Top 300",
     columns = c(starts_with("top300"))
@@ -260,6 +272,8 @@ cfb_state_summary %>%
     label = "In-State",
     columns = c(starts_with("in_state"))
   ) %>% 
+  
+  # Styling Table
   tab_style(
     style = list(
       cell_borders(
@@ -272,25 +286,51 @@ cfb_state_summary %>%
       columns = c(in_state_commits, top300_recruits)
     )
   ) %>% 
-  cols_align(
-    columns = 2:10,
-    align = "center"
+  tab_style(
+    style = cell_text(
+      size = px(36)
+    ),
+    locations = cells_title(
+      groups = "title"
+    )
   ) %>% 
-  tab_options(table.font.size = px(20)) %>%
+  tab_style(
+    style = cell_text(
+      size = px(24)
+    ),
+    locations = cells_body(
+      columns = c(state_name, where(is_integer))
+    )
+  ) %>% 
+  tab_style(
+    style = cell_text(
+      size = px(26)
+    ),
+    locations = list(
+      cells_column_labels(columns = everything()),
+      cells_column_spanners(spanners = everything())
+    )
+  ) %>% 
+  tab_options(
+    table.font.size = px(20),
+    table.border.top.color = "white",
+    table.border.bottom.color = "white",
+    heading.border.bottom.color = "white"
+    
+    ) %>%
   opt_table_font(
     font = list(
-      google_font(name = "Ubuntu"),
+      google_font(name = "Source Sans Pro"),
       "Sans", "Serif"
     )
   ) %>% 
-opt_css(
+  
+  
+  # Adding custom css
+  opt_css(
   css = '.top-3-img {
   width: 50px;
   height: 50px;}
-  
-  .gt_row gt_center {
-  display: flex;
-  justify-content: flex-start;}
 
   .top-3 {
   display: flex;
@@ -303,5 +343,9 @@ opt_css(
   margin: 3px 0px;
   }'
 )
+
+
+gtsave(gt_tbl, "Staying Home.png", vwidth = 2500)
+
 
 
