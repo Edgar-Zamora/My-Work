431b620b94c3532cded861f4bdefba628ee1a83e
Edgar-Zamora <edgarzamora2012@hotmail.com>
2022-04-11 23:39:49 -0700

    prep data for iterating

--- Tidymodels/MLB/get_attnd.R
+++ Tidymodels/MLB/get_attnd.R
@@ -14,14 +14,19 @@ library(mlbstatsR)
 # Getting mlb attendance data
 # https://www.baseball-reference.com/teams/SEA/2021-schedule-scores.shtml
 # https://www.baseball-reference.com/teams/SEA/2020-schedule-scores.shtml
+#'https://www.baseball-reference.com/teams/',{{team}}, '/' ,{{year}}, '-schedule-scores.shtml'
 
 
 # #div_team_schedule
 
-session <- bow("https://www.baseball-reference.com/teams/SEA/2021-schedule-scores.shtml", force = TRUE)
 
-
-scrape(session) %>% 
+mlbscrapR <- function(team, year) {
+  
+  url <- paste0("https://www.baseball-reference.com/teams/", team, "/", year, "-schedule-scores.shtml")
+  
+  session <- bow(url)
+  
+  scrape(session) %>% 
   html_node("#div_team_schedule") %>% 
   html_table() %>% 
   clean_names() %>% 
@@ -30,8 +35,18 @@ scrape(session) %>%
                                TRUE ~ "home"),
          walkoff = case_when(str_detect(w_l, "wo") ~ 1,
                              TRUE ~ 0),
-         w_l = str_remove_all(w_l, "-wo")) %>% 
-  select(-c(x, x_2)) %>% 
-  view()
+         w_l = str_remove_all(w_l, "-wo"),
+         mlb_season = year) %>% 
+  select(-c(x, x_2))
+}
+
+
+years <- c(2011:2021)
+mlb_teams <- c("LAA")
+
+
+map2_df(mlb_teams, years, mlbscrapR)
+
+
 
 
