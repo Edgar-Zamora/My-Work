0fc7c21f75fb29d1c8e6b6bc6fbc9ed6ccba9db6
Edgar-Zamora <edgarzamora2012@hotmail.com>
2022-04-13 00:39:04 -0700

    Prep iternation

--- Tidymodels/MLB/get_attnd.R
+++ Tidymodels/MLB/get_attnd.R
@@ -27,26 +27,45 @@ mlbscrapR <- function(team, year) {
   session <- bow(url)
   
   scrape(session) %>% 
-  html_node("#div_team_schedule") %>% 
-  html_table() %>% 
-  clean_names() %>% 
-  filter(tm != "Tm") %>% 
-  mutate(away_home = case_when(x_2 == "@" ~ "away",
-                               TRUE ~ "home"),
-         walkoff = case_when(str_detect(w_l, "wo") ~ 1,
-                             TRUE ~ 0),
-         w_l = str_remove_all(w_l, "-wo"),
-         mlb_season = year) %>% 
-  select(-c(x, x_2))
+    html_element("table") %>% 
+    html_table() %>%  
+    clean_names() %>% 
+    filter(tm != "Tm") %>%
+    separate(col = date, into = c('weekday', "month", "day"), sep = "\\s") %>%
+    mutate(away_home = case_when(x_2 == "@" ~ "away",
+                                 TRUE ~ "home"),
+           walkoff = case_when(str_detect(w_l, "wo") ~ 1,
+                               TRUE ~ 0),
+           forfeit = case_when(str_detect(w_l, "&V") ~ 1,
+                               TRUE ~ 0),
+           w_l = str_remove_all(w_l, "-wo|&V"),
+           extra_innings = case_when(inn == "" ~ 1,
+                                     TRUE ~ 0),
+           mlb_season = year,
+           across(attendance, parse_number),
+           across(weekday, ~str_remove_all(., "[:punct:]")),
+           month = match(month, month.abb),
+           date = ymd(paste0(year, "0", month, day)),
+           mlb_team = team) %>%
+    select(-c(x, x_2)) %>%
+    rename(
+      win_lose = w_l,
+      innings = inn,
+      win_lose_record = w_l_2,
+      day_night = d_n
+    )
 }
 
 
 years <- c(2011:2021)
-mlb_teams <- c("LAA")
+mlb_teams <- c("ARI", "ATL", "BAL", "BOS", "CHC", "CHW", "CIN",
+               "CLE", "COL", "DET", "HOU", "KCR", "LAA", "LAD",
+               "MIN", "MIA", "MIL", "NYM", "NYY", "OAK", "PHI",
+               "PIT", "SDP", "SFG", "SEA", "STL", "TBR", "TEX",
+               "TOR", "WSN")
 
 
-map2_df(mlb_teams, years, mlbscrapR)
-
+x <- map2(mlb_teams, years, mlbscrapR)
 
 
 
